4f38c6c7fd6aedfd45f2e2c8da39500368a5af47
Laxminarayan-Birle <laxminarayan.2022ca046@mnnit.ac.in>
2026-03-02 17:14:40 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -8,10 +9,23 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app=express();
+const __dirname = path.resolve();
 
+const PORT = process.env.PORT || 3000;
 app.use("/api/auth",authRoutes); 
 app.use("/api/auth",messageRoutes); 
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server  running on port number ${process.env.PORT}`);
+
+// for deployment
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+
+    });
+}
+app.listen(PORT,()=>{
+    console.log(`server  running on port number ${PORT}`);
 });
